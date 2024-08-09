8fe7ec70e7c6e0f4998a6400cab56c4baa8f23fe
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-08-09 14:04:58 +0100

    PartTimeEmployee

--- Main.java
+++ Main.java
@@ -40,6 +40,22 @@ class FullTimeEmployee extends Employee {
     }
 }
 
+class PartTimeEmployee extends Employee {
+    private int hoursWorked;
+    private double hourlyRate;
+
+    public PartTimeEmployee(String name, int id, int hoursWorked, double hourlyRate) {
+        super(name, id);
+        this.hoursWorked = hoursWorked;
+        this.hourlyRate = hourlyRate;
+    }
+
+    @Override
+    public double calculateSalary() {
+        return hoursWorked * hourlyRate;
+    }
+}
+
 
 
 public class Main {
