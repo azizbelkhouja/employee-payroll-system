72ec6370cc527fca4ec3a61e5f162fd7cdb6ec06
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-08-09 14:13:52 +0100

    Main.java

--- Main.java
+++ Main.java
@@ -90,6 +90,21 @@ class PayrollSystem {
 
 public class Main {
     public static void main(String[] args) {
+        PayrollSystem payrollSystem = new PayrollSystem();
 
+        FullTimeEmployee emp1 = new FullTimeEmployee("Steve Jobs", 101, 5000.0);
+        PartTimeEmployee emp2 = new PartTimeEmployee("Enzo Ferrari", 102, 30, 15.0);
+
+        payrollSystem.addEmployee(emp1);
+        payrollSystem.addEmployee(emp2);
+
+        System.out.println("Initial Employee Details:");
+        payrollSystem.displayEmployees();
+
+        System.out.println("\nRemoving Employee...");
+        payrollSystem.removeEmployee(101);
+
+        System.out.println("\nRemaining Employee Details:");
+        payrollSystem.displayEmployees();
     }
 }
