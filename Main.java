b662bc4cb99e7bf3c111a9350c6a012b0e752a61
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-08-09 13:59:45 +0100

    FullTimeEmployee

--- Main.java
+++ Main.java
@@ -26,7 +26,19 @@ abstract class Employee {
     }
 }
 
+class FullTimeEmployee extends Employee {
+    private double monthlySalary;
 
+    public FullTimeEmployee(String name, int id, double monthlySalary) {
+        super(name, id);
+        this.monthlySalary = monthlySalary;
+    }
+
+    @Override
+    public double calculateSalary() {
+        return monthlySalary;
+    }
+}
 
 
 
